8d124ed2463cce568efece7b799e56c8550fc2dd
julian1111111 <145719754+julian1111111@users.noreply.github.com>
2024-09-26 18:58:17 -0400

    Adds enemies, HP, returns to main menu on death

--- game-files/player/player.gd
+++ game-files/player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+var health = 10
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @onready var anim = get_node("AnimationPlayer")
@@ -34,3 +34,7 @@ func _physics_process(delta: float) -> void:
 		anim.play("Fall")
 
 	move_and_slide()
+	
+	if health <= 0:
+		queue_free()
+		get_tree().change_scene_to_file("res://main.tscn")
